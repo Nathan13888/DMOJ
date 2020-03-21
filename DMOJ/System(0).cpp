41053c10cd01ba6db123eb6659969302aff1f3b3
Nathan <29968201+Nathan13888@users.noreply.github.com>
2020-03-20 22:36:36 -0400

    First Backup

---
+++ DMOJ/System(0).cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    double n1, n2;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n >> n1 >> n2;
+    printf("%.9f\n", -n2 / n1 / n);
+}
---
+++ DMOJ/test.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    cout << true + true;
+    ios_base::sync_with_stdio(false);
+    cout << (char)0x48 << (char)0x65 << (char)0x6C << (char)0x6C << (char)0x6F << (char)0x2C
+         << (char)0x20 << (char)0x57 << (char)0x6F << (char)0x72 << (char)0x6C << (char)0x64 << (char)0x21;
+}
