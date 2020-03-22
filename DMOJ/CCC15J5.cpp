20fe1900e027e921e70910211bca737fbd359e41
Nathan <nathan13888@gmail.com>
2020-03-21 22:29:12 -0400

    March 21st Backup #2

---
+++ DMOJ/CCC15J5.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    //TODO: finish
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int K, N, P;
+    cin >> N >> K;
+
+    return 0;
+}
