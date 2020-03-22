1a2b459532ad91a8b09a11c8f50eb36be5525da9
Nathan <29968201+Nathan13888@users.noreply.github.com>
2020-03-21 22:07:17 -0400

    CCC14S2; dun gooft, is much easier than it is, over thought it ;(, will get fixed now

--- DMOJ/CCC16S2.cpp
+++ DMOJ/CCC16S2.cpp
@@ -9,13 +9,47 @@ int main()
     int Q, N;
     cin >> Q >> N;
     int dmo[N], peg[N];
+    for (int i = 0; i < N; i++)
+    {
+        int tmp;
+        cin >> tmp;
+        dmo[i] = tmp;
+    }
+    for (int i = 0; i < N; i++)
+    {
+        int tmp;
+        cin >> tmp;
+        peg[i] = tmp;
+    }
     sort(dmo, dmo + N);
     sort(peg, peg + N);
+    int r = N % 2, total = 0;
     if (Q == 1)
-    {
+    { // TODO-fix: need to filter the mid section of
+        //Question 1 : what is the minimum total speed,
+        for (int i = 0; i < (N - r) / 2; i++)
+        {
+            total += dmo[i];
+        }
+        for (int i = 0; i < (N - r) / 2; i++)
+        {
+            total += peg[i];
+        }
+        if (r == 1)
+            total += min(dmo[N / 2 - 1], peg[N / 2 - 1]);
     }
     else
-    {
+    { //Question 2 : what is the maximum total speed,
+        for (int i = 0; i < (N - r) / 2; i++)
+        {
+            total += dmo[N - i - 1];
+        }
+        for (int i = 0; i < (N - r) / 2; i++)
+        {
+            total += peg[N - i - 1];
+        }
+        if (r == 1)
+            total += max(dmo[N / 2 - 1], peg[N / 2 - 1]);
     }
 
     return 0;
