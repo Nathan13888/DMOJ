05bdc0a0138c86e8c622cb6ecdd92f84c2d3a8df
Nathan <29968201+Nathan13888@users.noreply.github.com>
2020-03-21 22:29:12 -0400

    March 21st Backup #2

--- DMOJ/CCC16S2.cpp
+++ DMOJ/CCC16S2.cpp
@@ -22,35 +22,18 @@ int main()
         peg[i] = tmp;
     }
     sort(dmo, dmo + N);
-    sort(peg, peg + N);
-    int r = N % 2, total = 0;
+    int total = 0;
     if (Q == 1)
-    { // TODO-fix: need to filter the mid section of
         //Question 1 : what is the minimum total speed,
-        for (int i = 0; i < (N - r) / 2; i++)
-        {
-            total += dmo[i];
-        }
-        for (int i = 0; i < (N - r) / 2; i++)
-        {
-            total += peg[i];
-        }
-        if (r == 1)
-            total += min(dmo[N / 2 - 1], peg[N / 2 - 1]);
-    }
+        sort(peg, peg + N);
     else
-    { //Question 2 : what is the maximum total speed,
-        for (int i = 0; i < (N - r) / 2; i++)
-        {
-            total += dmo[N - i - 1];
-        }
-        for (int i = 0; i < (N - r) / 2; i++)
-        {
-            total += peg[N - i - 1];
-        }
-        if (r == 1)
-            total += max(dmo[N / 2 - 1], peg[N / 2 - 1]);
-    }
+        //Question 2 : what is the maximum total speed,
+        sort(peg, peg + N, greater<int>());
+
+    for (int i = 0; i < N; i++)
+        total += max(dmo[i], peg[i]);
+
+    cout << total;
 
     return 0;
 }
