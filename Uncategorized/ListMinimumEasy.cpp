17d5aa5b987ce6baecac1e3866c717788acffad2
Nathan <nathan13888@gmail.com>
2020-03-25 23:43:25 -0400

    midnight updates 11:43pm

---
+++ Uncategorized/ListMinimumEasy.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    // You see, this problem is actuall a scam so just ignore the problem and just print the thing out ;'')
+
+    for (int i = 1; i <= n; i++)
+    {
+        cout << i << " ";
+    }
+
+    // int *arr = new int[n];
+    // for (int i = 0; i < n; i++)
+    //     cin >> arr[i];
+
+    // sort(arr, arr + n);
+
+    // bool first = true;
+    // for (size_t i = 0; i < n; i++)
+    // {
+    //     cout << arr[i] << " ";
+    // }
+
+    // delete arr;
+
+    return 0;
+}
