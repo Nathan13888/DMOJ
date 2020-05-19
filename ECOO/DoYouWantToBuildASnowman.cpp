e64e282511bc58c740f8d0419f1b2ec5b98121e9
Nathan13888 <29968201+Nathan13888@users.noreply.github.com>
2020-05-19 17:41:37 -0400

    finish ECOO2014P1

--- ECOO/DoYouWantToBuildASnowman.cpp
+++ ECOO/DoYouWantToBuildASnowman.cpp
@@ -2,6 +2,26 @@
 using namespace std;
 
 // https://dmoj.ca/problem/mockecoo14p1https://dmoj.ca/problem/mockecoo14p1
+vector<string> lines = {
+    // 16 lines here index 0-15
+    "          |",
+    "       \\  |  /",
+    "        \\ | /",
+    "         \\|/",
+    "       XXXXXXX",
+    "      X       X",
+    "     X  O   O  X",
+    "    X     V     X",
+    "W   X  X     X  X",
+    " \\   X  XXXXX  X",
+    "  \\   X       X",
+    "   \\   XXXXXXX",
+    "    \\ X       X---",
+    "     X    O    X  \\",
+    "    X           X  \\",
+    "     XXXXXXXXXXX    \\",
+};
+
 int main()
 {
     ios_base::sync_with_stdio(false);
@@ -14,43 +34,84 @@ int main()
         int N;
         cin >> N;
 
-        vector<string> lines = {
-            // 12 lines here index 0-11
-            "          |",
-            "       \  |  /",
-            "        \ | /",
-            "         \|/",
-            "       XXXXXXX",
-            "      X       X",
-            "     X  O   O  X",
-            "    X     V     X",
-            "W   X  X     X  X",
-            " \   X  XXXXX  X",
-            "  \   X       X",
-            "   \   XXXXXXX",
-            "    \ X       X---",
-            "     X    O    X  \\",
-            "    X           X  \\",
-            "     XXXXXXXXXXX    \\",
-        };
-
         if (N == 1)
         {
-                lines.push_back("      OOOO OOOO      M");
-                lines.push_back("      OOOO OOOO");
+            lines.push_back("      OOOO OOOO      M");
+            lines.push_back("      OOOO OOOO");
         }
         else
         {
-            int ln = 0, gap=(N-2)*2-1;
-            int h,w;
+            int h = 4, w = 6, curW = 10;
 
-            while (N--)
+            // do one less than N--
+            while (--N)
             {
+                int gap = 0;
+                // add torso
+                for (int x = 0; x < h; x++)
+                {
+                    string tmp;
+                    // add gap
+                    for (int i = 0; i < gap; i++)
+                    {
+                        tmp += " ";
+                    }
+                    tmp += "X";
+                    // add gap 2
+                    for (int i = 0; i < w - 1; i++)
+                    {
+                        tmp += " ";
+                    }
+                    // add button or not
+                    if (x == 0 || x == h - 1)
+                        tmp += " ";
+                    else
+                        tmp += "0";
+                    // add gap 3
+                    for (int i = 0; i < w - 1; i++)
+                    {
+                        tmp += " ";
+                    }
+                    tmp += "X";
+                }
+                // add joint
+                string joint;
+                // add gap
+                for (int i = 0; i < gap + 1; i++)
+                {
+                    joint += " ";
+                }
+                // add Xs
+                for (int i = 0; i < (w - 1) * 2 - 1; i++)
+                {
+                    joint += "X";
+                }
+
                 h++;
                 w++;
+                if (w > curW) // add 2 spaces to the left side of all the previous lines
+                    for (long unsigned int i = 0; i < lines.size(); i++)
+                    {
+                        for (int j = 0; j < w - curW; j++)
+                            lines[0] = " " + lines[0];
+                    }
+
+                curW = max(curW, w);
             }
+            // Add the right hand to index 16
+            lines[16] += "     M";
             // Legs
+            int cnt = w - 4;
+            string gap2 = "";
+            while (cnt--)
+                gap2 += " ";
+            lines.push_back(gap2 + "OOOO OOOO");
+            lines.push_back(gap2 + "OOOO OOOO");
         }
+        // output lines
+        for (auto s : lines)
+            cout << s << "\n";
+
         if (cases != 1)
             cout << "\n";
     }
