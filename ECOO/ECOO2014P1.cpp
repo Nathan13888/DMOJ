06a4f1a57da22452a5bbc6bf07d9a6eb588e5ade
Nathan13888 <29968201+Nathan13888@users.noreply.github.com>
2020-05-19 18:35:05 -0400

    separate solution into functions. clean solution and add test method

--- ECOO/ECOO2014P1.cpp
+++ ECOO/ECOO2014P1.cpp
@@ -24,6 +24,111 @@ vector<string> lines = {
 
 vector<string> model;
 
+int h = 4, w = 5, curW = 10;
+
+void gen(int N)
+{
+    // do one less than N--
+    while (--N)
+    {
+        if (w > curW) // add 2 spaces to the left side of all the previous lines
+            for (long unsigned int i = 0; i < lines.size(); i++)
+            {
+                for (int j = 0; j < w - curW; j++)
+                    lines[0] = " " + lines[0];
+            }
+        curW = max(curW, w);
+
+        int gap = curW - w;
+        // add torso
+        for (int x = 0; x < h; x++)
+        {
+            string tmp;
+            // add gap
+            for (int i = x; i < gap; i++)
+            {
+                tmp += " ";
+            }
+            tmp += "X";
+            // add gap 2
+            for (int i = 0; i < w + x - 1; i++)
+            {
+                tmp += " ";
+            }
+            // add button or not
+            if (x == 0 || x == h - 1)
+                tmp += " ";
+            else
+                tmp += "0";
+            // add gap 3
+            for (int i = 0; i < w + x - 1; i++)
+            {
+                tmp += " ";
+            }
+            tmp += "X";
+            lines.push_back(tmp);
+        }
+        // add joint
+        string joint;
+        // add gap
+        for (int i = 0; i < gap + 1; i++)
+        {
+            joint += " ";
+        }
+        // add Xs
+        for (int i = 0; i < (curW - 1) * 2 + 1; i++)
+        {
+            joint += "X";
+        }
+
+        lines.push_back(joint);
+
+        h++;
+        w++;
+    }
+    // Add the right hand to index 16
+    lines[16] += "     M";
+    // Legs
+    int cnt = w - 4;
+    string gap2 = "";
+    while (cnt--)
+        gap2 += " ";
+    lines.push_back(gap2 + "OOOO OOOO");
+    lines.push_back(gap2 + "OOOO OOOO");
+}
+
+void run(int N)
+{
+    if (N == 1)
+    {
+        lines.push_back("      OOOO OOOO      M");
+        lines.push_back("      OOOO OOOO");
+    }
+    else
+        gen(N);
+
+    // output lines
+    for (auto s : lines)
+        cout << s << "\n";
+
+    // RESET vector "lines"
+    lines = model;
+}
+
+void runTest() {
+    int Ns[] = {
+        1,3,5
+    };
+
+    int count = 0;
+
+    for(int N:Ns){
+        run(N);
+        if (++count != Ns.length())
+            cout << "\n";
+    }
+}
+
 int main()
 {
     ios_base::sync_with_stdio(false);
@@ -31,102 +136,20 @@ int main()
 
     model = lines;
 
+    // TEST
+    runTest();
+    return 0;
+    //
+
     int cases = 5;
+    int N;
 
     while (cases--)
     {
-        int N;
         cin >> N;
-
-        if (N == 1)
-        {
-            lines.push_back("      OOOO OOOO      M");
-            lines.push_back("      OOOO OOOO");
-        }
-        else
-        {
-            int h = 4, w = 6, curW = 10;
-
-            // do one less than N--
-            while (--N)
-            {
-                if (w > curW)
-                { // add 2 spaces to the left side of all the previous lines
-                    for (long unsigned int i = 0; i < lines.size(); i++)
-                    {
-                        for (int j = 0; j < w - curW; j++)
-                            lines[0] = " " + lines[0];
-                    }
-                    curW = max(curW, w);
-                }
-
-                int gap = curW - w;
-                // add torso
-                for (int x = 0; x < h; x++)
-                {
-                    string tmp;
-                    // add gap
-                    for (int i = x; i < gap; i++)
-                    {
-                        tmp += " ";
-                    }
-                    tmp += "X";
-                    // add gap 2
-                    for (int i = 0; i < w + x - 1; i++)
-                    {
-                        tmp += " ";
-                    }
-                    // add button or not
-                    if (x == 0 || x == h - 1)
-                        tmp += " ";
-                    else
-                        tmp += "0";
-                    // add gap 3
-                    for (int i = 0; i < w + x - 1; i++)
-                    {
-                        tmp += " ";
-                    }
-                    tmp += "X";
-                    lines.push_back(tmp);
-                }
-                // add joint
-                string joint;
-                // add gap
-                for (int i = 0; i < gap + 1; i++)
-                {
-                    joint += " ";
-                }
-                // add Xs
-                for (int i = 0; i < (curW - 1) * 2 + 1; i++)
-                {
-                    joint += "X";
-                }
-
-                lines.push_back(joint);
-
-                h++;
-                w++;
-            }
-            // Add the right hand to index 16
-            lines[16] += "     M";
-            // Legs
-            int cnt = w - 4;
-            string gap2 = "";
-            while (cnt--)
-                gap2 += " ";
-            lines.push_back(gap2 + "OOOO OOOO");
-            lines.push_back(gap2 + "OOOO OOOO");
-        }
-        // output lines
-        for (auto s : lines)
-            cout << s << "\n";
-
+        run(N);
         if (cases != 1)
-        {
             cout << "\n";
-            // RESET vector "lines"
-            lines = model;
-        }
     }
 
     return 0;
